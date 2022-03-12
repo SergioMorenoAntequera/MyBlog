3b57a6ac18c3c08c8845d106bae103a64882fbf4
SergioMorenoAntequera <seranmoreno500@gmail.com>
2022-03-12 20:30:15 +0100

    Reaction types added

---
+++ src/types/reactions.js
@@ -0,0 +1,12 @@
+
+const TYPES = Object.freeze({
+    "like": "ReactionType.LIKE",
+    "disLike": "ReactionType.DISLIKE"
+})
+
+const ATTACHED_TO_TYPES = Object.freeze({
+    "post": "ReactionAttachedToType.POST",
+    "comment": "ReactionAttachedToType.COMMENT"
+})
+
+export {TYPES, ATTACHED_TO_TYPES}
