9ba19ed18221a12fe608cf085f7257f91a7485ba
Sergio Moreno <sergiomoreno@realtracksystems.com>
2022-04-05 10:03:28 +0200

    Env checker + updating env_template

--- src/api/_config.js
+++ src/api/_config.js
@@ -5,7 +5,7 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 import { getFirestore } from "firebase/firestore";
 
-const firebase = initializeApp({
+const FIREBASE_CONFIG = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -13,7 +13,16 @@ const firebase = initializeApp({
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-})
+}
+const UNVALID_CONFIG = Object.keys(FIREBASE_CONFIG).filter(it=>!FIREBASE_CONFIG[it])
+if(UNVALID_CONFIG.length > 0) {
+    throw Error("UNABLE TO READ FIREBASE CONFIGURATION FROM ENV VARIABLES: \n"+
+        UNVALID_CONFIG.map(configName => `${configName} = ${FIREBASE_CONFIG[configName]}\n`)
+    )
+}
+
+
+const firebase = initializeApp(FIREBASE_CONFIG)
 
 
 const BASE_URL = process.env.BASE_URL || "https://jsonplaceholder.typicode.com"
