ef495c7d79f15dff179a70b3ce4e7b4bbe6364fd
SergioMorenoAntequera <seranmoreno500@gmail.com>
2022-04-15 14:52:12 +0200

    Avatar Styled components

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import "./Avatar.scss"
+import * as S from "./Avatar.styled"
 
-export default function Avatar({user, redirect = true, onClick}) {
+export default function Avatar({className, user, redirect = true, onClick}) {
 
-    if(!user) return <></>  
-    return (<div className='Avatar' onClick={onClick}>
+    if(!user) return <></>
+    return (<S.Avatar className={`Avatar ${className}`} onClick={onClick}>
+
       <Link to={`/user/${user.uid}`} style={{pointerEvents:`${redirect ? 'unset' : 'none'}`}}>
-        <img className='Avatar_Image' referrerPolicy="no-referrer" src={user.photoURL} alt={user.displayName} ></img>
+        <img className='Image' referrerPolicy="no-referrer" src={user.photoURL} alt={user.displayName} ></img>
       </Link>
-    </div>)
+
+    </S.Avatar>)
 }
---
+++ src/components/Avatar/Avatar.styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Avatar = styled.div`
+
+    display: inline-block;
+    
+    .Image {
+        border-radius: 100%;
+        width: 30px;
+        height: 30px;
+    }
+    
+`;
+
+
