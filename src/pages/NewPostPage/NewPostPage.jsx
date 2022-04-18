d3d2cc2e6db7176d7b51730db9b1e6e2e5750372
SergioMorenoAntequera <seranmoreno500@gmail.com>
2022-04-18 20:55:51 +0200

    Imlpementing new Form to create posts

--- src/pages/NewPostPage/NewPostPage.jsx
+++ src/pages/NewPostPage/NewPostPage.jsx
@@ -1,18 +1,36 @@
 import { H1, Avatar } from 'components'
 import * as authAPI from "api/auth"
 import * as S from './NewPostPage.styled' 
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Button } from 'components'
+import { useDispatch } from 'react-redux'
+import { createPost } from 'actions/postsActions'
 
 export default function NewPostPage() {
   
+  const dispatch = useDispatch()
   const { user } = authAPI.useUser()
+  const postTitle = useRef()
+  const postBody = useRef()
 
-  return (<div>
+  function crateNewPost(event) {
+    event.preventDefault()
+    let newPost = {
+      title: postTitle.current.value,
+      body: postBody.current.value
+    }
+    dispatch(createPost(user?.uid, newPost))
+  }
+
+  return (<S.NewPostPage>
 
     <H1>
       <Avatar user={user} redirect={false}/>
       New Post 
     </H1>
-    <S.TitleInput placeholder="My new Adventure..."/>
-  </div>)
+    <S.TitleInput ref={postTitle} placeholder="My new Adventure..."/>
+    <S.BodyTextArea ref={postBody} placeholder="Lorem..."/>
+
+    <Button variant="contained" onClick={crateNewPost}> POST </Button>
+  </S.NewPostPage>)
 }
--- src/pages/NewPostPage/NewPostPage.styled.js
+++ src/pages/NewPostPage/NewPostPage.styled.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+export const NewPostPage = styled.div`
+  min-height: 90vh;
+  display: grid;
+  grid-template-rows: auto auto 1fr auto;
+`;
+
 export const TitleInput = styled.input`
     width: 100%;
     padding: .5em 0;
@@ -11,6 +17,16 @@ export const TitleInput = styled.input`
 
     font-size: ${({theme})=>theme.typography.fontSizes.l};
     
+    :focus {
+        outline: none;
+    }
+`;
+
+export const BodyTextArea = styled.textarea`
+    border: none;
+    margin: 2em 0;
+    width: 100%;
+
     :focus {
         outline: none;
     }
