eb320cb991ac568acd924e61fa277722d853cbff
SergioMorenoAntequera <seranmoreno500@gmail.com>
2022-04-25 22:22:19 +0200

    cleaning avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import * as authAPI from "api/auth"
 import * as S from "./Avatar.styled"
 
 
 export default function Avatar({className, user, redirect = true, onClick}) {
 
-    const { user:auxUser } = authAPI.useUser()
-    user = user ?? auxUser
-
+  
     let hide = user ? "" : "hidden"
     return (<S.Avatar className={`Avatar ${className} ${hide}`} onClick={onClick}>
       <Link to={`/user/${user?.uid}`} style={{pointerEvents:`${redirect ? 'unset' : 'none'}`}}>
