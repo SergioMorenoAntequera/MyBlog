80ebb8e9b76563eaa313b274b8e428d9931ac7fe
SergioMorenoAntequera <seranmoreno500@gmail.com>
2022-05-05 22:38:54 +0200

    Updating model to use TimeStamp

--- src/types/Model.js
+++ src/types/Model.js
@@ -1,10 +1,13 @@
+import { Timestamp } from 'firebase/firestore';
 import { v4 as uuid } from 'uuid';
 
 export default class Model {
 
     id = ""
+    createdAt = new Timestamp()
 
     constructor() {
         this.id = uuid()
+        this.createdAt = Timestamp.now()
     }
 }
