c89651ec19d57fc5620b938c1385f5b07da688a6
SergioMorenoAntequera <seranmoreno500@gmail.com>
2023-02-22 23:14:20 +0100

    [FIX] Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-import { SITE_PRE_URL } from "./tsconfig.json";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://SergioMorenoAntequera.github.io',
-  base: SITE_PRE_URL,
+  base: '/MyBlog',
   server: { port: 1234, host: true},
   integrations: [mdx(), sitemap(), react(), tailwind()]
 });
